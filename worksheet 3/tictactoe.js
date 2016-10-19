9b88a1dca80827ee48acbc8652e0e939cac2823d
raquelduran <raquel.ferduran@gmail.com>
2016-10-19 10:11:49 +0200

    added the file which will contain tictactoe game

---
+++ worksheet 3/tictactoe.js
@@ -0,0 +1,19 @@
+/*
+ EJERCICIOS – DEFINICI N DE OBJETOS POR EL USUARIO Ó
+Haciendo uso de P.O.O. Modela los siguientes enunciados. En cada uno de ellos se te
+pide la implementaci n de un peque o juego haciendo uso de JavaScript. En este momento no ó ñ
+debes pensar en la representaci n gr fica del juego, s lo c ntrate en el dise o de la clase ó á ó é ñ
+que permita implementar el juego en cuesti n con todas sus posibilidades y casuistica. ó
+Posteriormente dotaremos a estos juegos de una representaci n gr fica adecuada y se ó á
+representar n en pantalla, pero en este momento deben ser independientes de sta (tan s lo á é ó
+tendr n una interfaz b sica en pruebas)
+
+2) Tres en Raya. El juego del tres en raya consiste en un tablero de 3x3, en el que
+sucesivamente dos jugadores van marcando casillas hasta conseguir tener sus tres
+marcas en l nea, pudiendo ser sta horizontal, vertical o en diagonal. Se pide por tanto í é
+la implementaci n de ste juego teniendo en cuenta: ó é
+1. El juego es pensado para ser usado por dos usuarios.
+2. El juego finaliza cuando los dos seleccionan sus 3 marcas, o bien uno consigue
+antes las tres en l nea. 
+
+*/
